672b7d4abe9b2202ed2b0df49b02103118af34a5
Anthony D. Blaom <anthony.blaom@gmail.com>
2026-02-02 15:03:34 +1300

    add cross-reference to confusion_counts_at_thresholds

--- src/functions.jl
+++ src/functions.jl
@@ -190,7 +190,8 @@ $footer
 
 $(DOC_ROC())
 
-For a method with checks, see [`roc_curve`](@ref).
+For a method with checks, see [`roc_curve`](@ref). See also
+[`Functions.confusion_counts_at_thresholds`](@ref).
 
 """
 function roc_curve(scores, y, positive_class)
@@ -238,7 +239,8 @@ $footer
 
 $(DOC_PRECISION_RECALL())
 
-See also [`precision_recall_curve`](@ref), which includes some checks.
+See also [`precision_recall_curve`](@ref), which includes some checks, and
+[`Functions.confusion_counts_at_thresholds`](@ref).
 
 """
 function precision_recall_curve(scores, y, positive_class)
